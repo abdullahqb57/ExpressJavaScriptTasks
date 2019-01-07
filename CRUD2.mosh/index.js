c369f3c0b899e59f7f594e82931603c615101587
abdullahqb57 <44702244+abdullahqb57@users.noreply.github.com>
2019-01-07 21:26:14 +0530

    Update index.js

--- CRUD2.mosh/index.js
+++ CRUD2.mosh/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const Joi = require('joi')
+const Joi = require('joi');
 const port = process.env.PORT || 3000;
 app.use(express.json());
 const courses = [
